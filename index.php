332117ef2c5dae1fb81f6c0cd448c42502f262ae
Adrien78 <adrien.morla66@gmail.com>
2017-11-09 18:46:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<?php
+$url = "http://tonyarchambeau.com/feed/"; /* insérer ici l'adresse du flux RSS de votre choix */
+$rss = simplexml_load_file($url);
+echo '<ul>';
+foreach ($rss->channel->item as $item){
+ $datetime = date_create($item->pubDate);
+ $date = date_format($datetime, 'd M Y, H\hi');
+ echo '<li><a href="'.$item->link.'">'.utf8_decode($item->title).'</a><p>'.utf8_decode($item->description).'</p> ('.$date.')</li>';
+}
+echo '</ul>';
+?>
